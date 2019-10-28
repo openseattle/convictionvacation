a87ac06f7003a74cde94c92378073d30629429ce
Stephen Chan <stephen.s.c.chan@gmail.com>
2019-10-27 19:29:00 -0700

    Add multiple convictions scenario test logic for calculator (with very limited test cases)

--- src/logic/EligibilityTimelineCalculator.test.js
+++ src/logic/EligibilityTimelineCalculator.test.js
@@ -26,16 +26,16 @@ let singleConvictionTestData = [
     ["Felony B 10 Years has passed since relevant date", CrimeClassification.FELONY_CLASS_B, false, false, "1990-01-01", "2019-01-01", true],
     ["Felony C 5 Years has passed since relevant date", CrimeClassification.FELONY_CLASS_C, false, false, "2013-01-01", "2019-01-01", true],
     // Non-Vacatable test cases
-    ["Misdemeanor 3 Years has passed since relevant date", CrimeClassification.MISDEMEANOR, false, false, "2016-01-15", "2019-01-01", false],
-    ["Misdemeanor with Domestic Violence, 5 Years has passed since relevant date", CrimeClassification.MISDEMEANOR, true, false, "2014-03-01", "2019-01-01", false],
-    ["Misdemeanor with DUI, 10 Years has passed since relevant date", CrimeClassification.MISDEMEANOR, false, true, "2009-02-01", "2019-01-01", false],
-    ["Misdemeanor with Domestic Violence & DUI, 10 Years has passed since relevant date", CrimeClassification.MISDEMEANOR, true, true, "2009-02-01", "2019-01-01", false],
-    ["Gross Misdemeanor 3 Years has passed since relevant date", CrimeClassification["GROSS MISDEMEANOR"], false, false, "2016-07-01", "2019-01-01", false],
-    ["Gross Misdemeanor with Domestic Violence, 5 Years has passed since relevant date", CrimeClassification["GROSS MISDEMEANOR"], true, false, "2014-03-01", "2019-01-01", false],
-    ["Gross Misdemeanor with DUI, 10 Years has passed since relevant date", CrimeClassification["GROSS MISDEMEANOR"], false, true, "2009-07-01", "2019-01-01", false],
-    ["Gross Misdemeanor with Domestic Violence & DUI, 10 Years has passed since relevant date", CrimeClassification["GROSS MISDEMEANOR"], false, true, "2009-07-01", "2019-01-01", false],
-    ["Felony B 10 Years has passed since relevant date", CrimeClassification.FELONY_CLASS_B, false, false, "2010-01-01", "2019-01-01", false],
-    ["Felony C 5 Years has passed since relevant date", CrimeClassification.FELONY_CLASS_C, false, false, "2015-12-01", "2019-01-01", false],
+    ["Misdemeanor 3 Years has not passed since relevant date", CrimeClassification.MISDEMEANOR, false, false, "2016-01-15", "2019-01-01", false],
+    ["Misdemeanor with Domestic Violence, 5 Years has not passed since relevant date", CrimeClassification.MISDEMEANOR, true, false, "2014-03-01", "2019-01-01", false],
+    ["Misdemeanor with DUI, 10 Years has not passed since relevant date", CrimeClassification.MISDEMEANOR, false, true, "2009-02-01", "2019-01-01", false],
+    ["Misdemeanor with Domestic Violence & DUI, 10 Years has not passed since relevant date", CrimeClassification.MISDEMEANOR, true, true, "2009-02-01", "2019-01-01", false],
+    ["Gross Misdemeanor 3 Years has not passed since relevant date", CrimeClassification["GROSS MISDEMEANOR"], false, false, "2016-07-01", "2019-01-01", false],
+    ["Gross Misdemeanor with Domestic Violence, 5 Years has not passed since relevant date", CrimeClassification["GROSS MISDEMEANOR"], true, false, "2014-03-01", "2019-01-01", false],
+    ["Gross Misdemeanor with DUI, 10 Years has not passed since relevant date", CrimeClassification["GROSS MISDEMEANOR"], false, true, "2009-07-01", "2019-01-01", false],
+    ["Gross Misdemeanor with Domestic Violence & DUI, 10 Years has not passed since relevant date", CrimeClassification["GROSS MISDEMEANOR"], false, true, "2009-07-01", "2019-01-01", false],
+    ["Felony B 10 Years has not passed since relevant date", CrimeClassification.FELONY_CLASS_B, false, false, "2010-01-01", "2019-01-01", false],
+    ["Felony C 5 Years has not passed since relevant date", CrimeClassification.FELONY_CLASS_C, false, false, "2015-12-01", "2019-01-01", false],
 
     // Test cases for conviction logic for 1984-07-01 cutoff date
     ["Felony B Before 1st July, 1984", CrimeClassification.FELONY_CLASS_B, false, false, "1984-06-30", "2019-01-01", false],
@@ -52,5 +52,41 @@ test.each(singleConvictionTestData)(
         let actualCalculatorOutput = calculator.calculate(input);
 
         expect(actualCalculatorOutput.getConviction(TEST_ID).vacatable).toBe(expectedVacatable);
-    },);
-    
+    }
+);
+
+// Parameterized test table with the following format:
+// Array of[Crime Name, Crime Classification, Is Domestic Violence Related, Is DUI Related, Crime Relevant Date], 
+//   Calculation Date, Expected Conviction Vacatable
+
+let multipleConvictionsTestData = [
+    // Test cases for latest conviction date is within the requirements
+    [
+        [
+            ["Felony B no new conviction in the past 10 years", CrimeClassification.FELONY_CLASS_B, false, false, "1990-01-01"],
+            ["Latest conviction", CrimeClassification.MISDEMEANOR, false, false, "2008-12-30"]
+        ],
+        "2019-01-01", true
+    ],
+    [
+        [
+            ["Felony C no new conviction in the past 5 years", CrimeClassification.FELONY_CLASS_C, false, false, "1990-01-01"],
+            ["Latest conviction", CrimeClassification.MISDEMEANOR, false, false, "2014-01-01"]
+        ],
+        "2019-01-01", true
+    ],
+];
+
+test.each(multipleConvictionsTestData)(
+    'Multiple convictions=%s, calculationDate="%s"',
+    (convictions, calculationDate, expectedVacatable) => {
+        let calculator = new ConvictionCalculator();
+        let convictionInputs = convictions.map((conviction, index) => {
+            return new ConvictionInput(TEST_ID + index, ...conviction);
+        });
+        let input = new CalculatorInput(calculationDate, convictionInputs);
+        let actualCalculatorOutput = calculator.calculate(input);
+        
+        expect(actualCalculatorOutput.getConviction(TEST_ID + "0").vacatable).toBe(expectedVacatable);
+    }
+);
