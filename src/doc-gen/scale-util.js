439ad705b44385c9c399b27a3f23bbfb56ee037f
Vritant Bhardwaj <vritoku@gmail.com>
2019-11-23 15:14:38 -0800

    added dim units

--- src/doc-gen/scale-util.js
+++ src/doc-gen/scale-util.js
@@ -24,39 +24,56 @@ function GenHTML(form_obj, fields_arr, new_dimension) {
     const scale_util = new ScaleUtil(
         form_obj.dimension.width, form_obj.dimension.height,
         new_dimension.width, new_dimension.height);
-
+    
     //loop over all pages    
-    form_pages.reduce(
-        (acc, page_obj, page_no) => {
+    const pages = form_pages.reduce(
+            (acc, page_obj, page_no) => {
 
-            acc += genOpenPageDiv(page_obj.id, page_obj.url);
-            
-            //loop over all fields
-            acc += fields_arr.reduce(
-                (acc, field_obj) => {
-                    //only process fields in current page
-                    if(field_obj.page_number === page_no) {
-                        acc += genField(field_obj);
-                    }
-                    return acc;
-                },
-                ""
-            );
-            acc += "</div>"
-            return acc;
-        },
-        ""
+                acc += genOpenPageDiv(page_obj.id, page_obj.url);
+                
+                //loop over all fields
+                acc += fields_arr.reduce(
+                    (acc, field_obj) => {
+                        //only process fields in current page
+                        if(field_obj.page_number === page_no) {
+                            acc += genField(field_obj, scale_util);
+                        }
+                        return acc;
+                    },
+                    ""
+                );
+                acc += "</div>"
+                return acc;
+            },
+            ""
     )
+
+    const body = `
+        <body>
+            ${pages}
+        </body>
+    `;
+
+    const shared_styles = getSharedPrintableStyles(getPageCSS(dimension.width, dimension.height))
+
+    const html = `
+        <html>
+            ${shared_styles}
+            ${body}
+        </html>
+    `
+
+    return html;
 }
 
-function genField(field_obj) {
-    var width = field_obj.top_right.x - field_obj.bottom_left.x;
-    var height = field_obj.top_right.y - field_obj.bottom_left.y;
-    var top = field_obj.top_right.y;
-    var left = field_obj.bottom_left.x;
+function genField(field_obj, scale_util) {
+    var width = scale_util.scaleWidth(field_obj.top_right.x - field_obj.bottom_left.x);
+    var height = scale_util.scaleHeight(field_obj.top_right.y - field_obj.bottom_left.y);
+    var top = scale_util.getNewYOffset(field_obj.page_no, field_obj.top_right.y);
+    var left = scale_util.getNewXOffset(field_obj.page_no, field_obj.bottom_left.x);
     return `
         <svg id="${field_obj.id}" class="text-box"
-            style="top:${top}px;left:${left}px"  width="${width}" height="${height}" viewBox="0 0 ${width} ${height}">
+            style="top:${top}px;left:${left}px"  width="${width}in" height="${height}in" viewBox="0 0 ${width}in ${height}in">
             <text x="0" y="75%" textLength="100%">
                 <!-- populate value here -->
             </text>
@@ -70,11 +87,43 @@ function genOpenPageDiv(id, bg_image_url) {
             id="${id}",
             background_img:"${bg_image_url}"
         >
+    `
+}
 
-        <style>
-            .text-box {
-                position: absolute;
+function genOpenPageDiv(id, bg_image_url) {
+    return `
+        <div
+            id="${id}"
+            background_img:"${bg_image_url}"
+            class="page"
+        >       
+    `
+}
+
+function getSharedPrintableStyles(page_style) {
+    return `
+        <stlye>
+            @media print {
+                ${page_style},
+                ${getTextBoxCSS()}
             }
         </style>
+    `
+}
+
+function getTextBoxCSS() {
+    return `
+        .text-box {
+            position: absolute;
+        }
+    `;
+}
+
+function getPageCSS(width, height) {
+    return `
+        .page {
+            width: ${width}in;
+            height: ${height}in;
+        }
     `
 }
