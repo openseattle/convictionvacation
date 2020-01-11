6fd4d4168e46a43f8fcd6a9f9e12638edda6b1ee
zach.lamb <zach.lamb@regiscompany.com>
2020-01-11 11:45:41 -0800

    moved analytics to root component

--- src/App.js
+++ src/App.js
@@ -7,12 +7,10 @@ import ConvictionCalculator from './components/ConvictionCalculator';
 import './App.css';
 const trackingId = "UA-156075348-1";
 
-
-React.useEffect(() => {
-  ReactGA.initialize(trackingId);
-})
-
 function App() {
+  React.useEffect(() => {
+    ReactGA.initialize(trackingId);
+  })
   return (
     <div>
       <header className="App-header">
