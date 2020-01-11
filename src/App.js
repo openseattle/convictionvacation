c5c6af9078487b12aa6034e804c7c144fd2997f6
Zachary Lamb <zachlamb94@gmail.com>
2020-01-11 12:00:31 -0800

    Revert "Bug google anat test"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import ReactGA from 'react-ga';
-
 
 import ConvictionCalculator from './components/ConvictionCalculator';
 
 import './App.css';
-const trackingId = "UA-156075348-1";
 
 function App() {
-  React.useEffect(() => {
-    ReactGA.initialize(trackingId);
-  })
   return (
     <div>
       <header className="App-header">
