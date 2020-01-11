3c8281fbf0bc4ac7a6fe5694a6f0dc0049c16462
zach.lamb <zach.lamb@regiscompany.com>
2020-01-11 11:43:20 -0800

    switched analytics to be on different branch

--- src/components/Conviction.jsx
+++ src/components/Conviction.jsx
@@ -3,9 +3,6 @@ import { Button, Checkbox, Icon, Input, Select, Table } from 'semantic-ui-react'
 
 import CrimeClassification from '../logic/type/CrimesClassifications';
 
-import ReactGA from 'react-ga';
-const trackingId = "UA-156075348-1";
-
 const convictionClassificationOptions = [
   {
     key: CrimeClassification.FELONY_CLASS_B,
@@ -81,10 +78,6 @@ const Conviction = ({
       window.removeEventListener('resize', onResize);
     };
   }, []);
-
-  React.useEffect(() => {
-    ReactGA.initialize(trackingId);
-  })
 
   return (
     <Table.Row>
