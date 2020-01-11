4a261a1452808e3891a015593684cf0351c2b1b5
zach.lamb <zach.lamb@regiscompany.com>
2020-01-11 11:43:20 -0800

    switched analytics to be on different branch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import ReactGA from 'react-ga';
+
 
 import ConvictionCalculator from './components/ConvictionCalculator';
 
 import './App.css';
+const trackingId = "UA-156075348-1";
+
+
+React.useEffect(() => {
+  ReactGA.initialize(trackingId);
+})
 
 function App() {
   return (
