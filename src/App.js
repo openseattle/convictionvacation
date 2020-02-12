ec5250c1786952b693b2cfc145234abc635db94e
Jessi Shank <jessishank1@gmail.com>
2020-02-11 19:55:12 -0800

    can't actually use NODE_ENV, swapping to REACT_APP_ENV

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ import ConvictionCalculator from './components/ConvictionCalculator';
 import './App.css';
 
 
-
 import ReactGA from 'react-ga';
 const trackingId = "UA-156075348-1";
 
 const BetaWarning = () => {
-    if (process.env.NODE_ENV === 'beta') {
+    if (process.env.REACT_APP_ENV === 'beta') {
         return (<div className="App-beta-warning">
             <p>
                 You're in beta mode
